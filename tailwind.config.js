164e74e408f4fc47bb9acc2f444ec8cb4e55af85
Brandon Martinez <me@brandonmartinez.com>
2024-03-08 04:49:16 +0000

    Footer animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./themes/cloudchat/layout/**/*.njk"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "cloudchat-bubble": {
+          "0%": { opacity: 0 },
+          "50%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "cloudchat-bubble-1":
+          "cloudchat-bubble 6s ease 0s infinite alternate both",
+        "cloudchat-bubble-2":
+          "cloudchat-bubble 6s ease 4s infinite alternate both",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
