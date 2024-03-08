1f713a0776b0b464eaf169f2b4dec323c2f7e86e
Brandon Martinez <me@brandonmartinez.com>
2024-03-08 20:41:38 +0000

    Renamed icon folder, updated social section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,36 @@ module.exports = {
           "50%": { opacity: 0 },
           "100%": { opacity: 1 },
         },
+        heartbeat: {
+          from: {
+            transform: "scale(1)",
+            "transform-origin": "center center",
+            "animation-timing-function": "ease-out",
+          },
+          "10%": {
+            transform: "scale(0.91)",
+            "animation-timing-function": "ease-in",
+          },
+          "17%": {
+            transform: "scale(0.98)",
+            "animation-timing-function": "ease-out",
+          },
+          "33%": {
+            transform: "scale(0.87)",
+            "animation-timing-function": "ease-in",
+          },
+          "45%": {
+            transform: "scale(1)",
+            "animation-timing-function": "ease-out",
+          },
+        },
       },
       animation: {
         "cloudchat-bubble-1":
           "cloudchat-bubble 6s ease 0s infinite alternate both",
         "cloudchat-bubble-2":
           "cloudchat-bubble 6s ease 4s infinite alternate both",
+        heartbeat: "heartbeat 1s ease-in-out both",
       },
     },
   },
